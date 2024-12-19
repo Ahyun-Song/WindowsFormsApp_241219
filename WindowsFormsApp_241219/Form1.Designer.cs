fb5aa9dad13d4317c4364aaf220f349fbbba2862
Ahyun-Song <yoon9113@naver.com>
2024-12-19 10:35:16 +0900

    Add project files.

---
+++ WindowsFormsApp_241219/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApp_241219
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
+            this.radioButton_true = new System.Windows.Forms.RadioButton();
+            this.radioButton_false = new System.Windows.Forms.RadioButton();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(13, 13);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(599, 28);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // button_input
+            // 
+            this.button_input.Location = new System.Drawing.Point(629, 12);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(159, 28);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "input";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // radioButton_true
+            // 
+            this.radioButton_true.AutoSize = true;
+            this.radioButton_true.Location = new System.Drawing.Point(13, 48);
+            this.radioButton_true.Name = "radioButton_true";
+            this.radioButton_true.Size = new System.Drawing.Size(64, 22);
+            this.radioButton_true.TabIndex = 2;
+            this.radioButton_true.TabStop = true;
+            this.radioButton_true.Text = "true";
+            this.radioButton_true.UseVisualStyleBackColor = true;
+            this.radioButton_true.Click += new System.EventHandler(this.radioButton_true_Click);
+            // 
+            // radioButton_false
+            // 
+            this.radioButton_false.AutoSize = true;
+            this.radioButton_false.Location = new System.Drawing.Point(119, 47);
+            this.radioButton_false.Name = "radioButton_false";
+            this.radioButton_false.Size = new System.Drawing.Size(71, 22);
+            this.radioButton_false.TabIndex = 3;
+            this.radioButton_false.TabStop = true;
+            this.radioButton_false.Text = "false";
+            this.radioButton_false.UseVisualStyleBackColor = true;
+            this.radioButton_false.Click += new System.EventHandler(this.radioButton_false_Click);
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox_result.Location = new System.Drawing.Point(12, 75);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(775, 355);
+            this.textBox_result.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.radioButton_false);
+            this.Controls.Add(this.radioButton_true);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.RadioButton radioButton_true;
+        private System.Windows.Forms.RadioButton radioButton_false;
+        private System.Windows.Forms.TextBox textBox_result;
+    }
+}
+
---
+++ WindowsFormsApp_241219/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_241219
+{
+    public partial class Form1 : Form
+    {
+        bool input;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            bool userInput = true;
+
+            /*if (PlayGame(userInput))
+            {
+                textBox_result.Text = "승리";
+            }
+            else
+            {
+                textBox_result.Text = "패배";
+            }*/
+        }
+
+        public bool PlayGame(bool userInput)
+        {
+            Random random = new Random();
+            int coinTossResult = random.Next(2);
+            bool randomBool = (coinTossResult == 1);
+            return userInput == randomBool;
+
+        }
+
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            // 입력 텍스트를 가져옴
+            string input = textBox_input.Text.Trim();
+            bool userInput;
+
+            // textBox_input 값을 우선적으로 처리
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (bool.TryParse(input, out userInput))  // 입력값이 true/false인지 확인
+                {
+                    if (PlayGame(userInput))
+                    {
+                        ProcessInput(userInput);
+                    }
+                    else
+                    {
+                        textBox_result.Text = "true 또는 false를 입력하세요.";
+                    }
+                }
+                else
+                {
+                    textBox_result.Text = "값을 선택하거나 입력하세요.";
+                }
+            }
+            /*else
+            {
+                // 라디오 버튼 값 처리
+                if (radioButton_true.Checked)
+                {
+                    userInput = true;
+                    if (PlayGame(userInput))
+                    {
+                        textBox_result.Text = "승리";
+                    }
+                    else
+                    {
+                        textBox_result.Text = "패배";
+                    }
+                }
+                else if (radioButton_false.Checked)
+                {
+                    userInput = false;
+                    if (PlayGame(userInput))
+                    {
+                        textBox_result.Text = "승리";
+                    }
+                    else
+                    {
+                        textBox_result.Text = "패배";
+                    }
+                }
+                else
+                {
+                    textBox_result.Text = "값을 선택하거나 입력하세요.";
+                }
+            }*/
+        }
+
+        private void radioButton_true_Click(object sender, EventArgs e)
+        {
+            textBox_input.Text = "true";
+            ProcessInput(true);
+        }
+
+        private void radioButton_false_Click(object sender, EventArgs e)
+        {
+            textBox_input.Text = "false";
+            ProcessInput(false);
+        }
+
+        private void ProcessInput(bool userInput)
+        {
+            if (PlayGame(userInput))
+            {
+                textBox_result.Text = "승리";
+            }
+            else
+            {
+                textBox_result.Text = "패배";
+            }
+        }
+    }
+}
